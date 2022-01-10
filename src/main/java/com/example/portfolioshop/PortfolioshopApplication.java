334aa61968ce8da8375f82181ef58f9cc1d1ddab
web <tmdghks0914@naver.com>
2022-01-10 22:06:16 +0900

    Initial commit

---
+++ src/main/java/com/example/portfolioshop/PortfolioshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfolioshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioshopApplication.class, args);
+    }
+
+}
